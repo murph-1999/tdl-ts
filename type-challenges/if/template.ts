9bf6433a3a53ee719b1d83358c060dc3a89ed0d7
murph-1999 <647694376@qq.com>
2022-09-06 20:37:54 +0800

    09 if

--- type-challenges/if/template.ts
+++ type-challenges/if/template.ts
@@ -3,10 +3,10 @@
  * @version:
  * @Author: Murphy
  * @Date: 2022-09-06 20:20:33
- * @LastEditTime: 2022-09-06 20:36:20
+ * @LastEditTime: 2022-09-06 20:37:38
  */
 type If<C extends boolean, T, F> = C extends true ? T : F
 
 
 // 严格模式与非严格模式下的区别
-// type t = null extends true ? "true" : "false"
+// type t = null extends true ? "true " : "false"
